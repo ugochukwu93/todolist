b0004c88315447bda72668f2fa8e1812f46502de
Ugochukwu Nwadike <nwadikeugochukwu93@gmail.com>
2022-02-26 21:08:48 +0100

    updated the design of the todo app

--- todo.js
+++ todo.js
@@ -2,26 +2,29 @@ let todoInput = document.querySelector(".todo-input");
 let todoButton = document.querySelector(".todo-button");
 let todoList = document.querySelector(".todo-list");
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
+let date = document.getElementById("date")
+
+date.innerHTML = new Date();
 
 function addTodo() {
     if(todoInput.value != ""){
-    let todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
-    let newTodo = document.createElement("li");
-    newTodo.innerHTML = todoInput.value;
-    newTodo.classList.add("todo-item");
-    todoDiv.appendChild(newTodo);
-    saveLocalTodos(todoInput.value);
-    let completedButton = document.createElement("button");
-    completedButton.innerHTML = '<i class="fas fa-check"></i>';
-    completedButton.classList.add("complete-btn");
-    todoDiv.appendChild(completedButton);
-    let trashButton = document.createElement("button");
-    trashButton.innerHTML = 'x';
-    trashButton.classList.add("trash-btn");
-    todoDiv.appendChild(trashButton);
-    todoList.appendChild(todoDiv);
-    todoInput.value = "";
+        let todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+        let newTodo = document.createElement("li");
+        newTodo.innerHTML = todoInput.value;
+        newTodo.classList.add("todo-item");
+        todoDiv.appendChild(newTodo);
+        saveLocalTodos(todoInput.value);
+        let completedButton = document.createElement("button");
+        completedButton.innerHTML = '<i class="fas fa-check"></i>';
+        completedButton.classList.add("complete-btn");
+        todoDiv.appendChild(completedButton);
+        let trashButton = document.createElement("button");
+        trashButton.innerHTML = 'x';
+        trashButton.classList.add("trash-btn");
+        todoDiv.appendChild(trashButton);
+        todoList.appendChild(todoDiv);
+        todoInput.value = "";
     }  
     else{
         alert("Cannot add blank to-do");
